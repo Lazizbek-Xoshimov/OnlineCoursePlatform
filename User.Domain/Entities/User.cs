b4ea4eba163658970e05ddacbcebc62069eeee1e
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2025-07-29 16:21:59 +0500

    Add project files.

---
+++ User.Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace UserService.Domain.Entities;
+
+public class User
+{
+    public short Id { get; set; }
+    public string Name { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
