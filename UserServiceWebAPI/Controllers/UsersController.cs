e48ecca35ac776f523d5e70647b9e251d1ea6767
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2025-08-01 19:31:12 +0500

    WebApi Layer written for UserService

---
+++ UserServiceWebAPI/Controllers/UsersController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserService.Application.DTOs.UserDTOs;
+using UserService.Application.Interfaces;
+
+namespace UserServiceWebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UsersController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        var users = await _userService.RetrieveAllAsync();
+        return Ok(users);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetByIdAsync(short id)
+    {
+        var user = await _userService.RetrieveByIdAsync(id);
+        return Ok(user);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync([FromBody] UserForCreationDTO dto)
+    {
+        var user = await _userService.CreateAsync(dto);
+        return Ok(user);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> PutAsync(short id, [FromBody] UserForUpdateDTO dto)
+    {
+        var user = await _userService.ModifyAsync(id, dto);
+        return Ok(user);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteAsync(short id)
+    {
+        var user = await _userService.RemoveAsync(id);
+        return Ok(user);
+    }
+}
---
+++ UserServiceWebAPI/Program.cs
@@ -0,0 +1,42 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using UserService.Application.Interfaces;
+using UserService.Application.Mappers;
+using UserService.Infrastructure.AddDbContext;
+using UserService.Infrastructure.Interfaces;
+using UserService.Infrastructure.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService.Application.Services.UserService>();
+
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
