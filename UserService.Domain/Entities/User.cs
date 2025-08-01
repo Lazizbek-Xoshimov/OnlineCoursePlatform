dd8a25a5aad8823ff0f11d1ae2a29483fce42a3f
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2025-08-01 19:31:47 +0500

    Domain Layer written for UserService

---
+++ UserService.Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace UserService.Domain.Entities;
+
+public class User
+{
+    public short Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
