61706c6cc424afb501659ac757174bacf63c051d
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2025-08-03 16:47:28 +0500

    Added CourseRepository

---
+++ CourseService.DAL/Contacts/ICourseRepositoy.cs
@@ -0,0 +1,13 @@
+﻿using CourseService.DAL.Models;
+
+namespace CourseService.DAL.Contacts;
+
+public interface ICourseRepositoy
+{
+    public Task<Course> InsertAsync(Course course);
+    public Task<Course> SelectByIdAsync(short id);
+    public IQueryable<Course> SelectAll();
+    public Task<Course> UpdateAsync(Course course);
+    public Task<bool> DeleteAsync(short id);
+    public Task<bool> SaveChangeAsync();
+}
---
+++ CourseService.DAL/Repositories/CourseRepository.cs
@@ -0,0 +1,55 @@
+﻿using CourseService.DAL.Contacts;
+using CourseService.DAL.Data;
+using CourseService.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseService.DAL.Repositories;
+
+public class CourseRepository : ICourseRepositoy
+{
+    private readonly AppDbContext _dbContext;
+    private readonly DbSet<Course> _dbSet;
+
+    public CourseRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _dbSet = dbContext.Set<Course>();
+    }
+
+    public async Task<bool> DeleteAsync(short id)
+    {
+        var course = await _dbSet.FirstOrDefaultAsync(c => c.Id == id);
+        if (course == null)
+            return false;
+
+        _dbSet.Remove(course);
+        return true;
+    }
+
+    public async Task<Course> InsertAsync(Course course)
+    {
+        var result = await _dbSet.AddAsync(course);
+        return result.Entity;
+    }
+
+    public async Task<bool> SaveChangeAsync()
+    {
+        return await _dbContext.SaveChangesAsync() > 0;
+    }
+
+    public IQueryable<Course> SelectAll()
+    {
+        return _dbSet;
+    }
+
+    public async Task<Course> SelectByIdAsync(short id)
+    {
+        return await _dbSet.FirstOrDefaultAsync(c => c.Id == id);
+    }
+
+    public async Task<Course> UpdateAsync(Course course)
+    {
+        var result = _dbSet.Update(course);
+        return result.Entity;
+    }
+}
