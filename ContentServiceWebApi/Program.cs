d0a207e77e6b7a523da6d0161d6468bc86d8f775
Lazizbek-Xoshimov <lazizbekxoshimov55@gmail.com>
2025-08-03 15:09:50 +0500

    fix

--- ContentServiceWebApi/Program.cs
+++ ContentServiceWebApi/Program.cs
@@ -1,3 +1,8 @@
+using ContentServiceWebApi.Data;
+using ContentServiceWebApi.Features.Contents.Mappers;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,28 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configuring with MongoDb
+builder.Services.Configure<ContentDatabaseSettings>(
+    builder.Configuration.GetSection("ContentDatabaseSettings"));
+
+// Creating a MongoClient instance
+builder.Services.AddSingleton<IMongoClient>(_ =>
+{
+    var connectionString = builder.Configuration.GetSection("ContentDatabaseSettings:ConnectionString").Value;
+
+    return new MongoClient(connectionString);
+});
+builder.Services.AddSingleton<IMongoClient>(_ =>
+{
+    var settings = new MongoClientSettings()
+    {
+        Scheme = ConnectionStringScheme.MongoDB,
+        Server = new MongoServerAddress("localhost", 27017)
+    };
+
+    return new MongoClient(settings);
+});
 
 var app = builder.Build();
 
